c514b2a5ef7ecb2dc418beaa1eba1cdb74fed1e2
Meddy Menzikoff <m.menzikoff@oneclickflare.fr>
2021-06-21 12:16:16 +0200

    Fix: MR Feedbacks
    Signed-off-by: Meddy Menzikoff <m.menzikoff@oneclickflare.fr>

--- keyple-plugins/android-nfc/src/main/kotlin/org/eclipse/keyple/plugin/android/nfc/AndroidNfcPluginFactory.kt
+++ keyple-plugins/android-nfc/src/main/kotlin/org/eclipse/keyple/plugin/android/nfc/AndroidNfcPluginFactory.kt
@@ -13,4 +13,10 @@ package org.eclipse.keyple.plugin.android.nfc
 
 import org.eclipse.keyple.core.common.KeyplePluginExtensionFactory
 
+/**
+ * Interface for the Nfc Plugin Factory. Use AndroidNfcPluginFactoryProvider to retrieve
+ * an instance of it.
+ *
+ * @since 2.0
+ */
 interface AndroidNfcPluginFactory : KeyplePluginExtensionFactory
